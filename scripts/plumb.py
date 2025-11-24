ba720eeb4c043632829e48bf3f87fcc9877db138
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-11-24 16:23:39 +0300

    scripts/plumb.py: adds to scm with requirements file

---
+++ scripts/plumb.py
@@ -0,0 +1,107 @@
+import argparse
+import pdfplumber
+import csv
+from typing import List
+
+
+def read_args():
+    parser = argparse.ArgumentParser(description="Extract tables from PDF files")
+    parser.add_argument(
+        "--input",
+        type=str,
+        required=True,
+        help="PDF file path",
+    )
+    return parser.parse_args()
+
+
+def read_tables(path):
+    tables: List[List[List[List[str | None]]]] = []
+    with pdfplumber.open(path) as input:
+        for page in input.pages:
+            try:
+                page_tables = page.extract_tables()
+                if page_tables:
+                    tables.extend(page_tables)
+            except Exception:
+                pass
+    return tables
+
+
+def check_cell_types(tables):
+    for i, table in enumerate(tables):
+        for j, row in enumerate(table):
+            for k, cell in enumerate(row):
+                if not isinstance(cell, str):
+                    print(f"table {i} row {j} column {k} is not string but {type(cell).__name__}")
+    return tables
+
+
+def stringify_cells(tables) -> List[List[List[List[str | None]]]]:
+    for i, table in enumerate(tables):
+        for j, row in enumerate(table):
+            for k, cell in enumerate(row):
+                tables[i][j][k] = cell if cell is not None else ""
+    return tables
+
+
+def reverse_line_wrap(tables) -> List[List[List[List[str | None]]]]:
+    for i, table in enumerate(tables):
+        for j, row in enumerate(table):
+            for k, cell in enumerate(row):
+                tables[i][j][k] = cell.replace("\r\n", "\n").replace("\n", " " if j == 0 else ", ")
+    return tables
+
+
+def continued_tables(tables: List[List[List[List[str | None]]]]):
+    contd_tables: List[List[List[List[str | None]]]] = [[]]
+    last_table_header = []
+    for table in tables:
+        for j, row in enumerate(table):
+            if j == 0 and row != last_table_header:
+                last_table_header = row
+                contd_tables.append([])
+            contd_tables[-1].append(row)
+    return contd_tables
+
+
+def filter_short_tables(tables, threshold) -> List[List[List[List[str | None]]]]:
+    filtered = []
+    for table in tables:
+        if len(table) >= threshold:
+            filtered.append(table)
+    return filtered
+
+
+def filter_empty_header_cell_tables(tables: List[List[List[List[str | None]]]]):
+    filtered = []
+    for table in tables:
+        if table[0].count("") == 0:
+            filtered.append(table)
+    return filtered
+
+
+def print_table_headers(tables):
+    for i, table in enumerate(tables):
+        print(f"#{i} [ {" | ".join(table[0])} ] rows={len(table) - 1}")
+
+
+def write_table(dst, table):
+    with open(dst, "w", newline="", encoding="utf-8") as output:
+        writer = csv.writer(output)
+        for row in table:
+            writer.writerow(row)
+
+
+args = read_args()
+tables = read_tables(args.input)
+tables = stringify_cells(tables)
+check_cell_types(tables)
+tables = reverse_line_wrap(tables)
+tables = continued_tables(tables)
+tables = filter_short_tables(tables, threshold=10)
+tables = filter_empty_header_cell_tables(tables)
+print_table_headers(tables)
+
+for i, table in enumerate(tables):
+    write_table(f"{args.input.replace(".pdf", "")}.table{i}.csv", table)
