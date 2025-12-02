eac489d0c62baf4f43c34a61daae9eb0c182d4c5
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-02 13:20:36 +0300

    pipeline.sh: adds venv setup and activation

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,9 @@
 #!/usr/local/bin/bash
+# shellcheck disable=SC1091
+
+test -d .venv || python 3 -m venv .venv
+test "$VIRTUAL_ENV" || . .venv/bin/activate
+pip install -r dev/requirements.txt
 
 # plumb
 for file in pdf/*.pdf; do
